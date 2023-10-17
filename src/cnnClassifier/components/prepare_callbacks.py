6ca0c38ae9651581b54992092c0f1ebe1ace3980
007kakashi <upendrasgosavi@gmail.com>
2023-10-17 14:58:30 +0530

    new model updated

--- src/cnnClassifier/components/prepare_callbacks.py
+++ src/cnnClassifier/components/prepare_callbacks.py
@@ -3,7 +3,7 @@ import urllib.request as request
 from zipfile import ZipFile
 import tensorflow as tf
 import time
-from cnnClassifier.entity.config_entity import PrepareCallbacksConfig
+from src.cnnClassifier.entity.config_entity import PrepareCallbacksConfig
 
 
 class PrepareCallback:
